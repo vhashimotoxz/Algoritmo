e0ff73a0ac672b199441bad88fddf8a0815c660c
victor h. alencar <129953996+vhashimotoxz@users.noreply.github.com>
2025-08-06 21:42:01 -0300

    add:

---
+++ teste.c
@@ -0,0 +1,121 @@
+#include <stdio.h>
+//ex 1
+/*
+int main()
+{
+    int n1, n2;
+    double media;
+    printf("Informe o primeiro numero:");
+    scanf("%d", &n1);
+    printf("Informe o segundo numero:");
+    scanf("%d", &n2);
+    media = (n1 + n2) / 2;
+    printf("A media eh: %.2lf\n", media);
+    return 0;
+}
+    */
+
+   #include <stdio.h>
+//ex 2
+/*
+    int main()
+    {
+        int numero, quadrado;
+        printf("Informe um numero: ");
+        scanf("%d", &numero);
+        quadrado = numero * numero;
+        printf("O quadrado do numero eh: %d\n", quadrado);
+        return 0;    
+    }
+        */
+
+        #include <stdio.h>
+        //ex 3
+        /*
+        int main()
+        {
+            double salario, novoSalario, reajuste;
+            printf("Informe o salario do funcionario:");
+            scanf("%lf", &salario);
+            reajuste = salario/100;
+            novoSalario = salario + reajuste;
+            printf("Novo salario: %.2lf\n", novoSalario);
+            return 0;
+        }
+            */
+
+            #include <stdio.h>
+            //ex 4
+            /*
+            int main()
+            {
+                double preco, desconto, novoPreco;
+                printf("Informe o preco do produto:");
+                scanf("%lf", &preco);
+                desconto = preco * 0.09;
+                novoPreco = preco - desconto;
+                printf("Novo preco: %.2lf\n", novoPreco);
+                return 0;
+            }
+                */
+
+               #include <stdio.h>
+               //ex 5
+               /*
+               int main()
+               {
+                char v1, v2, v3;
+                printf("Informe o primeiro caracter:");
+                scanf("%c", &v1);
+                printf("Informe o segundo caracter:");
+                scanf(" %c", &v2);
+                printf("Informe o terceiro caracter:");
+                scanf(" %c", &v3);
+                printf("%c %c %c\n", v1, v2, v3);
+                return 0;
+               }
+                */
+
+                #include <stdio.h>
+                //ex 6
+                /*
+                int main()
+                {
+                    int A, B, C, resultado;
+                    printf("Informe o valor de A:");
+                    scanf("%d", &A);
+                    printf("Informe o valor de B:");
+                    scanf("%d", &B);
+                    printf("Informe o valor de C");
+                    scanf("%d", &C);
+                    resultado = 2 * ((A - C)/8) - B * 5;
+                    printf("O resultado foi de: %d", resultado);
+                    return 0;
+                }
+                    */
+
+                    #include <stdio.h>
+                    //ex7
+                    /*
+                    int main()
+                    {
+                        int n1, n2 , soma, multi, sub, divi, resto;
+                        printf("Informe o valor de n1:");
+                        scanf("%d", &n1);
+                        printf("Informe o valor de n2:");
+                        scanf("%d", &n2);
+                        soma = n1 + n2;
+                        sub = n1 - n2;
+                        multi = n1 * n2;
+                        divi = n1 / n2;
+                        resto = n1 % n2;
+                        printf("soma: %d\n", soma);
+                        printf("sub: %d\n", sub);
+                        printf("multi: %d\n", multi);
+                        printf("divi: %d\n", divi);
+                        printf("resto: %d\n", resto);
+                        return 0;
+                    }
+                        */
+
+                        
