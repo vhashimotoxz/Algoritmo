a0b90b2f0094c16a8cc0b662fb6d706f5dfaea8b
victor h. alencar <129953996+vhashimotoxz@users.noreply.github.com>
2025-08-08 21:43:56 -0300

    add:

---
+++ teste2.c
@@ -0,0 +1,147 @@
+#include <stdio.h>
+/*
+//BATERIA DE EXERCICIOS DA AULA 08/08
+int main()
+{
+     int num;
+     printf("Informe um valor:");
+     scanf("%d", &num);
+     num += 1;
+     printf("Novo valor: %d\n", num);
+     return 0;
+}
+*/
+
+#include <stdio.h>
+/*
+int main()
+{
+     double raio, area, perimetro, pi;
+    printf("Informe o valor para o raio:");
+    scanf("%lf", &raio);
+    pi = 3.14159;
+    area = pi * raio * raio;
+    perimetro = 2 * pi * raio;
+    printf("Novo valor: %lf\n", area);
+    printf("Novo valor: %lf\n", perimetro);
+    return 0;
+}
+*/
+
+#include <stdio.h>
+/*
+int main()
+{
+     int dias, idade, viveu;
+     printf("Informe sua idade:");
+     scanf("%d", &idade);
+     dias = 365;
+     viveu = idade * dias;
+     printf("Novo valor: %d\n", viveu);
+     return 0;
+}
+*/
+
+#include <stdio.h>
+/*
+int main()
+{
+     double num, resultado;
+     printf("Informe um valor:");
+     scanf("%lf", &num);
+     resultado = num * 0.04;
+     printf("Novo valor: %lf\n", resultado);
+     return 0;
+}
+*/
+
+#include <stdio.h>
+/*
+int main()
+{
+     double nota1, nota2, nota3, media;
+     printf("Informe sua primeira nota:");
+     scanf("%lf", &nota1);
+     printf("Informe sua segunda nota:");
+     scanf("%lf", &nota2);
+     printf("Informe sua terceira nota:");
+     scanf("%lf", &nota3);
+     media = (nota1 + nota2 + nota3) / 3;
+     printf("O valor da media eh: %lf\n", media);
+     return 0;
+}
+*/
+
+#include <stdio.h>
+/*
+int main()
+{
+     double nota1, nota2, nota3, mediaPonderada;
+     printf("Informe sua primeira nota:");
+     scanf("%lf", &nota1);
+     printf("Informe sua segunda nota:");
+     scanf("%lf", &nota2);
+     printf("Informe sua terceira nota:");
+     scanf("%lf", &nota3);
+     mediaPonderada = (nota1 * 2 + nota2 * 3 + nota3 * 5) / 10;
+     printf("Valor final eh: %lf\n", mediaPonderada);
+     return 0;
+}
+*/
+
+#include <stdio.h>
+/*
+int main()
+{
+     double dP, tG, vm;
+     printf("Informe a sua distancia percorrida:");
+     scanf("%lf", &dP);
+     printf("Informe seu tempo gasto:");
+     scanf("%lf", &tG);
+     vm = dP / tG;
+     printf("A velocidade media foi de: %lf\n", vm);
+     return 0;
+}
+*/
+
+#include <stdio.h>
+/*
+int main()
+{
+     double carro, valorFinal;
+     printf("Informe um valor para o carro:");
+     scanf("%lf", &carro);
+     valorFinal = carro + carro * 0.28;
+     valorFinal += carro * 0.45;
+     printf("O valor final foi de: %lf\n", valorFinal);
+     return 0;
+}
+*/
+
+#include <stdio.h>
+/*
+int main()
+{
+     int num, valorFinal;
+     printf("Informe um valor:");
+     scanf("%d", &num);
+     valorFinal = (num / 10) % 10;
+     printf("O valor final foi de: %d\n", valorFinal);
+     return 0;
+}
+*/
+
+#include <stdio.h>
+/*
+int main()
+{
+     int dd, dia, mes, ano, final;
+     printf("Informe sua data de nascimento:");
+     scanf("%d", &dd);
+     ano = (dd % 100);
+     mes = (dd % 10000) / 100;
+     dia = dd/10000;
+     printf("Sua data de nascimento abreviada eh: %d %d %d\n", dia, mes, ano);
+     return 0;
+}
+*/
