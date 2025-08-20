81ef4ee19978573d18335c1cfdbced9c5a5fdd5f
victor h. alencar <129953996+vhashimotoxz@users.noreply.github.com>
2025-08-20 20:41:00 -0300

    add

---
+++ teste3.c
@@ -0,0 +1,154 @@
+#include <stdio.h>
+#include <math.h>
+//EXERCICIOS DA AULA COM A BIBLIOTECA DE MATEMATICA
+
+/*
+int main()
+{
+    double angulo, seno, cosseno, tangente;
+    printf("Informe um valor para o angulo:");
+    scanf("%lf", &angulo);
+    seno = sin(angulo);
+    cosseno = cos(angulo);
+    tangente = tan(angulo);
+    printf("Seno: %.2lf\n", seno);
+    printf("Cosseno: %.2lf\n", cosseno);
+    printf("Tangente: %.2lf\n", tangente);
+    return 0;
+}
+*/
+
+#include <stdio.h>
+#include <math.h>
+/*
+int main()
+{
+    double num, raiz;
+    printf("Informe um numero:");
+    scanf("%lf", &num);
+    raiz = sqrt(num);
+    printf("A raiz do numero informado eh: %2.lf\n", raiz);
+    return 0;
+}
+*/
+
+#include <stdio.h>
+#include <math.h>
+/*
+int main()
+{
+    double x, y, resultado;
+    printf("Informe o valor de X:");
+    scanf("%lf", &x);
+    printf("Informe o valor de Y:");
+    scanf("%lf", &y);
+    resultado = pow(x,y);
+    printf("O resultado eh: %2.lf\n", resultado);
+    return 0;
+}
+*/
+
+#include <stdio.h>
+#include <math.h>
+/*
+int main()
+{
+    double num;
+    printf("Informe um numero:");
+    scanf("%lf", &num);
+    printf("Floor: %lf\n", floor(num));
+    printf("Ceil: %lf\n", ceil(num));
+    printf("Round : %lf\n", round(num));
+    return 0;
+}
+*/
+
+#include <stdio.h>
+#include <math.h>
+/*
+int main()
+{
+    double num, quadrado, cubo;
+    printf("Informe um valor:");
+    scanf("%lf",&num);
+    quadrado = pow(num,2);
+    cubo = pow(num,3);
+    printf("O resultado eh: %.2lf\n %.2lf\n", quadrado, cubo);
+    return 0;
+}
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+/*
+int main()
+{
+    double num, num1, raizquadrada, aux;
+    printf("Informe o primeiro valor:");
+    scanf("%lf", &num);
+    printf("Informe o segundo valor:");
+    scanf("%lf", &num1);
+    aux = num + num1;
+    raizquadrada = sqrt(aux);
+    printf("O resultado eh: %.2lf\n", raizquadrada);
+    return 0;
+}
+*/
+
+#include <stdio.h>
+#include <math.h>
+/*
+int main()
+{
+    double termo, razao, ordem, posi;
+    printf("Informe um valor para o termo:");
+    scanf("%lf", &termo);
+    printf("Informe um valor para a razao:");
+    scanf("%lf", &razao);
+    printf("Informe um valor para a posicao:");
+    scanf("%lf", &posi);
+    ordem = termo + (posi - 1) * razao;
+    printf("O resultado eh: %.2lf\n", ordem);
+    return 0;
+}
+*/
+
+#include <stdio.h>
+#include <math.h>
+/*
+int main()
+{
+    double x1, x2, y1, y2, soma;
+    printf("Informe um valor para o x1:");
+    scanf("%lf", &x1);
+    printf("Informe um valor para o y1:");
+    scanf("%lf", &y1);
+    printf("Informe um valor para o x2:");
+    scanf("%lf", &x2);
+    printf("Informe um valor para o y2:");
+    scanf("%lf", &y2);
+    soma = sqrt ((pow(x2 - x1, 2)) + (pow(y2 - y1, 2)));
+    printf("O resultado eh: %.2lf\n", soma);
+    return 0;
+}
+*/
+
+#include <stdio.h>
+#include <math.h>
+/*
+int main()
+{
+    double a,b,c, aux1, aux2;
+    printf("Informe um valor para A:");
+    scanf("%lf", &a);
+    printf("Informe um valor para B:");
+    scanf("%lf", &b);
+    printf("Informe um valor para C:");
+    scanf("%lf", &c);
+    aux1 = pow((a + b), 2);
+    aux2 = pow((b + c), 2);
+    printf("Seu resultado eh: %.2lf\n %.2lf\n", aux1, aux2);
+    return 0;
+}
+*/    
